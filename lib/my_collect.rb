4f86e39efa351eb082cabb53eaaf3a611e5668f9
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 22:58:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   i = 0
   collection = []
   while i < collection.length
-    collection << yield(array[i].upcase)
+    collection << yield(array[i])
     i += 1
   end
   collection
